6e7001194958c0296dc8c0d38a9071b5cd330757
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-11-20 23:38:26 +0300

    Add files via upload

--- CollectionBinarySearchTree/BinarySearchTree.cs
+++ CollectionBinarySearchTree/BinarySearchTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,81 +7,391 @@ using System.Threading.Tasks;
 
 namespace CollectionBinarySearchTree
 {
-    public class BinarySearchTree<T> where T : IComparable<T>
+    public class BinarySearchTree<T> : IEnumerable<T> where T : IComparable<T>
     {
+        #region Private fields
+
+        private Node<T> _head;
+        private IComparer<T> _comparer;
+        private int _size;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of a Binary Tree
+        /// </summary>
+        public BinarySearchTree()
+        {
+            _head = null;
+            _size = 0;
+            _comparer = Comparer<T>.Default;
+        }
+
+        /// <summary>
+        /// Creates a new instance of a Binary Tree with initialized comparer
+        /// </summary>
+        public BinarySearchTree(IComparer<T> comparer)
+        {
+            _head = null;
+            _size = 0;
+            _comparer = comparer;
+        }
+
+        #endregion
+
         #region Properties
 
-        public Node<T> Root { get; set; } = null;
+        /// <summary>
+        /// Gets or sets the root of the tree (the top-most node)
+        /// </summary>
+        public Node<T> Root
+        {
+            get { return _head; }
+            set { _head = value; }
+        }
+
+        /// <summary>
+        /// Gets whether the tree is read-only
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets the number of elements stored in the tree
+        /// </summary>
+        public int Count
+        {
+            get { return _size; }
+        }
 
         #endregion
 
         #region Public methods
 
+        /// <summary>
+        /// Adds a new element to the tree
+        /// </summary>
         public void Insert(T value)
         {
-            var node = new Node<T>(value);
-            if (Root == null)
+            if (value == null)
             {
-                Root = node;
+                throw new ArgumentNullException($"{nameof(value)} is null.");
+            }
+
+            Node<T> node = new Node<T>(value);
+            Insert(node);
+        }
+
+        /// <summary>
+        /// Adds a node to the tree
+        /// </summary>
+        public void Insert(Node<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException($"{nameof(node)} is null.");
+            }
+
+            if (_head == null)
+            {
+                _head = node;
+                node.Tree = this;
+                _size++;
             }
             else
             {
-                var current = Root;
-                while (true)
+                if (node.Parent == null)
                 {
-                    int result = value.CompareTo(current.Value);
-                    if (result == 0)
+                    node.Parent = _head;
+                }
+
+                if (_comparer.Compare(node.Value, node.Parent.Value) <= 0)
+                {
+                    if (node.Parent.LeftChild == null)
                     {
-                        current.Value = value;
-                        return;
-                    }
-                    else if (result < 0)
-                    {
-                        current = current.Left;
-                        if (current == null)
-                        {
-                            return;
-                        }
+                        node.Parent.LeftChild = node;
+                        _size++;
+                        node.Tree = this;
                     }
                     else
                     {
-                        current = current.Right;
-                        if (current == null)
-                        {
-                            return;
-                        }
+                        node.Parent = node.Parent.LeftChild;
+                        Insert(node);
+                    }
+                }
+                else
+                {
+                    if (node.Parent.RightChild == null)
+                    {
+                        node.Parent.RightChild = node;
+                        _size++;
+                        node.Tree = this;
+                    }
+                    else
+                    {
+                        node.Parent = node.Parent.RightChild;
+                        Insert(node);
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Returns the first node in the tree with the parameter value.
+        /// </summary>
         public Node<T> Find(T value)
         {
-            var current = Root;
-
-            while (current.Value.CompareTo(value) != 0)
+            if (value == null)
             {
-                if (current.Value.CompareTo(value) > 0)
+                throw new ArgumentNullException($"{nameof(value)} is null.");
+            }
+
+            Node<T> node = _head;
+            while (node != null)
+            {
+                if (node.Value.Equals(value))
                 {
-                    current = current.Left;
+                    return node;
                 }
                 else
                 {
-                    current = current.Right;
-                }
-
-                if (current == null)
-                {
-                    return null;
+                    if (_comparer.Compare(value, node.Value) < 0)
+                    {
+                        node = node.LeftChild;
+                    }
+                    else
+                    {
+                        node = node.RightChild;
+                    }
                 }
             }
 
-            return current;
+            return null;
         }
 
+        /// <summary>
+        /// Returns whether a value is stored in the tree
+        /// </summary>
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+
+        /// <summary>
+        /// Removes a value from the tree and returns whether the removal was successful.
+        /// </summary>
         public void Remove(T value)
         {
+            Node<T> removeNode = Find(value);
+            Remove(removeNode);
+        }
 
+        /// <summary>
+        /// Removes a node from the tree and returns whether the removal was successful.
+        /// </summary>>
+        public bool Remove(Node<T> removeNode)
+        {
+            if (removeNode == null || removeNode.Tree != this)
+            {
+                return false;
+            }
+
+            bool wasHead = removeNode == _head;
+
+            if (Count == 1)
+            {
+                _head = null;
+                removeNode.Tree = null;
+
+                _size--;
+            }
+            else if (removeNode.IsLeaf)
+            {
+                if (removeNode.IsLeftChild)
+                {
+                    removeNode.Parent.LeftChild = null;
+                }
+                else
+                {
+                    removeNode.Parent.RightChild = null;
+                }
+
+                removeNode.Tree = null;
+                removeNode.Parent = null;
+
+                _size--;
+            }
+            else if (removeNode.ChildCount == 1)
+            {
+                if (removeNode.HasLeftChild)
+                {
+                    removeNode.LeftChild.Parent = removeNode.Parent;
+
+                    if (wasHead)
+                    {
+                        Root = removeNode.LeftChild;
+                    }
+
+                    if (removeNode.IsLeftChild)
+                    {
+                        removeNode.Parent.LeftChild = removeNode.LeftChild;
+                    }
+                    else
+                    {
+                        removeNode.Parent.RightChild = removeNode.LeftChild;
+                    }
+                }
+                else
+                {
+                    removeNode.RightChild.Parent = removeNode.Parent;
+
+                    if (wasHead)
+                    {
+                        Root = removeNode.RightChild;
+                    }
+
+                    if (removeNode.IsLeftChild)
+                    {
+                        removeNode.Parent.LeftChild = removeNode.RightChild;
+                    }
+                    else
+                    {
+                        removeNode.Parent.RightChild = removeNode.RightChild;
+                    }
+                }
+
+                removeNode.Tree = null;
+                removeNode.Parent = null;
+                removeNode.LeftChild = null;
+                removeNode.RightChild = null;
+
+                _size--;
+            }
+            else
+            {
+                Node<T> successorNode = removeNode.LeftChild;
+                while (successorNode.RightChild != null)
+                {
+                    successorNode = successorNode.RightChild;
+                }
+
+                removeNode.Value = successorNode.Value;
+
+                Remove(successorNode);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns an preorder-traversal enumerator for the tree values
+        /// </summary>
+        public IEnumerable<T> PreOrder()
+        {
+            return PreOrder(_head);
+        }
+
+        /// <summary>
+        /// Returns a postorder-traversal enumerator for the tree values
+        /// </summary>
+        public IEnumerable<T> PostOrder()
+        {
+            return PostOrder(_head);
+        }
+
+        /// <summary>
+        /// Returns an inorder-traversal enumerator for the tree values
+        /// </summary>
+        public IEnumerable<T> InOrder()
+        {
+            return InOrder(_head);
+        }
+
+        /// <summary>
+        /// Returns an enumerator to scan through the elements stored in tree.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrder().GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator to scan through the elements stored in tree.
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private IEnumerable<T> PreOrder(Node<T> node)
+        {
+            yield return node.Value;
+
+            if (node.LeftChild != null)
+            {
+                foreach (var n in PreOrder(node.LeftChild))
+                {
+                    yield return n;
+                }
+            }
+
+            if (node.RightChild != null)
+            {
+                foreach (var n in PreOrder(node.RightChild))
+                {
+                    yield return n;
+                }
+            }
+        }
+
+        private IEnumerable<T> InOrder(Node<T> node)
+        {
+            if (node.LeftChild != null)
+            {
+                foreach (var n in InOrder(node.LeftChild))
+                {
+                    yield return n;
+                }
+            }
+
+            yield return node.Value;
+
+            if (node.RightChild != null)
+            {
+                foreach (var n in InOrder(node.RightChild))
+                {
+                    yield return n;
+                }
+            }
+        }
+
+        private IEnumerable<T> PostOrder(Node<T> node)
+        {
+            if (node.LeftChild != null)
+            {
+                foreach (var n in PostOrder(node.LeftChild))
+                {
+                    yield return n;
+                }
+            }
+
+            if (node.RightChild != null)
+            {
+                foreach (var n in PostOrder(node.RightChild))
+                {
+                    yield return n;
+                }
+            }
+
+            yield return node.Value;
         }
 
         #endregion
--- CollectionBinarySearchTree/Node.cs
+++ CollectionBinarySearchTree/Node.cs
@@ -6,26 +6,150 @@ using System.Threading.Tasks;
 
 namespace CollectionBinarySearchTree
 {
+    /// <summary>
+    /// A Binary Tree node that holds an element and references to other tree nodes
+    /// </summary>
     public class Node<T> where T : IComparable<T>
     {
+        #region Private fields
+
+        private T _value;
+        private Node<T> _leftChild;
+        private Node<T> _rightChild;
+        private Node<T> _parent;
+        private BinarySearchTree<T> _tree;
+
+        #endregion
+
         #region Constructor
 
+        /// <summary>
+        /// Create a new instance of a Binary Tree node
+        /// </summary>
         public Node(T value)
         {
-            Value = value;
-            Left = null;
-            Right = null;
+            _value = value;
         }
 
         #endregion
 
-        #region Properties
+        #region Public properties
 
-        public T Value { get; set; }
+        /// <summary>
+        /// The value stored at the node
+        /// </summary>
+        public virtual T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
 
-        public Node<T> Left { get; set; }
+        /// <summary>
+        /// Gets or sets the left child node
+        /// </summary>
+        public virtual Node<T> LeftChild
+        {
+            get { return _leftChild; }
+            set { _leftChild = value; }
+        }
 
-        public Node<T> Right { get; set; }
+        /// <summary>
+        /// Gets or sets the right child node
+        /// </summary>
+        public virtual Node<T> RightChild
+        {
+            get { return _rightChild; }
+            set { _rightChild = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the parent node
+        /// </summary>
+        public virtual Node<T> Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the Binary Tree the node belongs to
+        /// </summary>
+        public virtual BinarySearchTree<T> Tree
+        {
+            get { return _tree; }
+            set { _tree = value; }
+        }
+
+        /// <summary>
+        /// Gets whether the node is a leaf (has no children)
+        /// </summary>
+        public virtual bool IsLeaf
+        {
+            get { return ChildCount == 0; }
+        }
+
+        /// <summary>
+        /// Gets whether the node is internal (has children nodes)
+        /// </summary>
+        public virtual bool IsInternal
+        {
+            get { return ChildCount > 0; }
+        }
+
+        /// <summary>
+        /// Gets whether the node is the left child of its parent
+        /// </summary>
+        public virtual bool IsLeftChild
+        {
+            get { return Parent != null && Parent.LeftChild == this; }
+        }
+
+        /// <summary>
+        /// Gets whether the node is the right child of its parent
+        /// </summary>
+        public virtual bool IsRightChild
+        {
+            get { return Parent != null && Parent.RightChild == this; }
+        }
+
+        /// <summary>
+        /// Gets the number of children this node has
+        /// </summary>
+        public virtual int ChildCount
+        {
+            get
+            {
+                int count = 0;
+
+                if (LeftChild != null)
+                {
+                    count++;
+                }
+
+                if (RightChild != null)
+                {
+                    count++;
+                }
+
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Gets whether the node has a left child node
+        /// </summary>
+        public virtual bool HasLeftChild
+        {
+            get { return LeftChild != null; }
+        }
+
+        /// <summary>
+        /// Gets whether the node has a right child node
+        /// </summary>
+        public virtual bool HasRightChild
+        {
+            get { return RightChild != null; }
+        }
 
         #endregion
     }
